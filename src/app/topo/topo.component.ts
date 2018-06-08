270c3ffb7e5ff5b0207cba9eeb0ec1d2fc34eb86
José Raul <jose.quadros@univille.br>
2018-06-08 14:31:33 -0300

    comitando projeto

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Connection} from "@angular/http";
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css']
+})
+export class TopoComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
